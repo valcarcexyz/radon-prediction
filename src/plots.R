3172645020dedb4b5eacc54f0142bdb6ee557b15
Diego Valcarce <dvalcarcer@gmail.com>
2022-04-22 17:04:22 +0200

    Real test data has been processed

--- src/plots.R
+++ src/plots.R
@@ -1,5 +1,7 @@
 library(ggplot2)     # plots
 library(lubridate)   # datetime parsing
+library(tidyr)       # data manipulation
+library(scales)      # dates
 
 ############################### time series load  #############################
 serie <- read.csv("../data/radon-data.csv")
@@ -75,8 +77,70 @@ ggplot(predictions[c(600:700),], aes(x = c(1:length(real)))) +
 ggsave("../figures/LSTM_forecasting_close.png", width = 10, height = 5)
 
 
+###################### test forecast results ##################################
+actions <- read.csv("../data/test_forecast_results/actions_ANN_predictor.csv")
+measures <- read.csv("../data/test_forecast_results/measures_ANN_predictor.csv")
+predictions <- read.csv("../data/test_forecast_results/predictions_ANN_predictor.csv")
 
+# parse all the dates
+actions$time <- as_datetime(actions$time)
+measures$time <- as_datetime(measures$time)
+predictions$time <- as_datetime(predictions$time)
 
+# we will change the names for simplicity
+colnames(measures)[which(names(measures)=="radon")] <- "real"
+colnames(predictions)[which(names(predictions)=="value")] <- "predictions"
 
+# the model was working for 4 days:
+start <- "2022-04-11 16:00"
+end <- "2022-04-15 00:00"
 
+actions <- subset(actions, time > start & time < end)[,c("time", "value")]
+measures <- subset(measures, time > start & time < end)[,c("time", "real")]
+predictions <- subset(predictions, 
+                      time > start & time < end)[,c("time", "predictions")]
 
+# combine them in a standalone table
+standalone <- merge(measures, predictions, by="time", all=TRUE)
+standalone[,"status"] <- NA
+
+last_status <- "Off"
+j <- 1
+for ( i in 1:nrow(actions) ){
+  current_date <- actions$time[i]
+  current_status <- actions$value[i]
+  while (current_date > standalone$time[j]) {
+    standalone$status[j] <- last_status
+    j <- j + 1
+  }
+  last_status <- current_status
+}
+
+# we now need to fill the gaps introduced by the left join
+standalone <- fill(standalone, value, .direction="up")
+standalone[is.na(standalone)] <- "Off" # the remaining (the firsts obervations)
+
+colors <- c("Predictions" = "red", "Real values" = "black")
+ggplot(standalone, aes(x = time)) +
+  geom_line(aes(y = predictions, color = "Predictions")) +
+  geom_line(aes(y = real, color = "Real values")) + 
+  scale_x_datetime(date_breaks = "1 day", labels = date_format("%d/%m")) +
+  labs(x = "Forecast",
+       y = "Radon level (Bq/m³)",
+       color = "Legend") + 
+  scale_color_manual(values = colors)
+ggsave("../figures/test_forecast.png", width = 10, height = 5)
+
+# on/off in the forecast
+colors <- c("On" = "red", "Off" = "black")
+ggplot(standalone, aes(x = time,
+                       y = real,
+                       group = 1,
+                       colour = status)) + 
+  geom_line() + 
+  scale_x_datetime(date_breaks = "1 day", labels = date_format("%d/%m")) + 
+  labs(x = "Time (10 minutes frequency)",
+       y = "Radon level (Bq/m³)",
+       color = "Ventilation status") + 
+  scale_color_manual(values = colors)
+ggsave("../figures/test_forecast_fan_status.png", width = 10, height = 5)
