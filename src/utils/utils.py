2c67481b69d417bad838644e3fca7bc3b0184e30
Diego Valcarce <d.valcarce@udc.es>
2022-04-27 09:15:06 +0200

    docstring updated

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,21 +1,27 @@
 import numpy as np
 import pandas as pd
+
+
 def generate_windows(data, x_len, y_len):
     """
-    Construye una matriz de ventanas dados los siguientes argumentos:
-    + data: list or array
-    + x_len : longitud de la ventana que se usará para predecir
-    + y_len: tamaño del horizonte
+    Builds the windowed version of data, given the length of x_len and y_len.
+
+    Params:
+    =======
+    @param data: list or array
+    @param x_len: int. Length of the training set
+    @param y_len: int. Length of the forecast
     """
     N = x_len + y_len
     return np.array([
         pd.Series(data).shift(i) for i in reversed(range(N))
-    ]).transpose()[N-1:, :]
+    ]).transpose()[N - 1:, :]
+
 
 def mix_data(data):
-    """Devuelve la matriz formateada"""
+    """Custom function to format properly the data"""
     complete = np.empty((list(data["radon"].shape) + [len(data.keys())]))
     keys = list(data.keys())
     for i in range(len(keys)):
-        complete[:,:,i] = data[keys[i]]
+        complete[:, :, i] = data[keys[i]]
     return complete
